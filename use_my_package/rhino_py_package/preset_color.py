2366fac2affacbb317fe34e0ed55b299117864f7
yoshioka <ysok.ysok@gmail.com>
2019-04-26 18:14:49 +0900

    [add] use package

---
+++ use_my_package/rhino_py_package/preset_color.py
@@ -0,0 +1,25 @@
+def red():
+    return (255,0,0)
+
+def green():
+    return (0,255,0)
+
+def blue():
+     return (0,0,255)
+
+def yellow():
+    return (255,255,0)
+
+def cyan():
+    return (0,255,255)
+
+def magenta():
+    return (255,0,255)
+
+def white():
+    return (255,255,255)
+
+def black():
+    return (0,0,0)
+
+### https://ja.wikipedia.org/wiki/RGB
---
+++ use_my_package/use_my_package.py
@@ -0,0 +1,6 @@
+from rhino_py_package import preset_color as pclr
+
+
+print dir(pclr)
+
+a = pclr.red()
